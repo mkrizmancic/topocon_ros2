e6bf040940751fc33dd44ce2759276d53bdadc15
Marko Krizmancic <marko.krizmancic@fer.hr>
2025-09-29 15:09:16 +0000

    Initial commit

---
+++ topocon/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'topocon'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='Decentralized algebraic connectivity estimation in ROS 2',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'node = topocon.node:main',
+        ],
+    },
+)
